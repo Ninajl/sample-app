f7a313ca6c6f900c1003204750cb09a949f5e29c
nina <ninajl@gmail.com>
2016-05-06 12:59:32 -0600

    start testing app response and selectmethod used on static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+# “Let’s test the Home page by
+# issuing a GET request
+# to the home action
+# and then making sure
+# we receive a ‘success’
+# status code in response.”
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
 end
